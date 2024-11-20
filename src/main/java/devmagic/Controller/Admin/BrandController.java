560d0115a28f6fc57c9c1dd6790a731bab0eda8c
nhp291 <nhp2901@gmail.com>
2024-11-21 06:48:04 +0700

    pull check code 1

--- src/main/java/devmagic/Controller/Admin/BrandController.java
+++ src/main/java/devmagic/Controller/Admin/BrandController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/brands")
+@RequestMapping("Brand")
 public class BrandController {
     @Autowired
     private BrandService brandsService;
