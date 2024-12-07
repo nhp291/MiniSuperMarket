a259b9de127a89df898fd969ffb52c8024d6a6fb
nhp291 <nhp2901@gmail.com>
2024-12-07 10:35:28 +0700

    done orders

--- src/main/java/devmagic/Service/OrderService.java
+++ src/main/java/devmagic/Service/OrderService.java
@@ -60,12 +60,22 @@ public class OrderService {
         return orderRepository.save(order);
     }
 
-    // Xóa đơn hàng (đánh dấu là đã xóa)
+    // Soft Delete: Đánh dấu đơn hàng là đã xóa
+//    public void markOrderAsDeleted(Integer id) {
+//        Order order = orderRepository.findById(id)
+//                .orElseThrow(() -> new RuntimeException("Order not found"));
+//
+//        order.setDeleted(true);
+//        orderRepository.save(order);  // Lưu lại thay đổi vào cơ sở dữ liệu
+//    }
+
+    // Hard Delete: Xóa đơn hàng khỏi cơ sở dữ liệu
     public void deleteOrder(Integer id) {
-        Order order = orderRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Order not found"));
-        order.setDeleted(true);
-        orderRepository.save(order);
+        if (orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Order not found");
+        }
     }
 
     // Tổng số đơn hàng
