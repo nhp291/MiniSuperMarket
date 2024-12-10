320a5b7bec9e089c2a1f1dad794ca36eb02ae17d
SuperMango23 <Hongphucmango@gmail.com>
2024-12-10 14:08:59 +0700

    Update CartController.java

--- src/main/java/devmagic/Controller/User/CartController.java
+++ src/main/java/devmagic/Controller/User/CartController.java
@@ -156,7 +156,7 @@ public class CartController {
         Order order = new Order();
         order.setAccount(new Account(accountId));
         order.setOrderDate(new Date());
-        order.setPaymentStatus("Thành Công");
+        order.setPaymentStatus("COMPLETED");
         order.setPaymentMethod(paymentMethod);
 
         order.setOrderDetails(cartItems.stream().map(cartItem -> {
